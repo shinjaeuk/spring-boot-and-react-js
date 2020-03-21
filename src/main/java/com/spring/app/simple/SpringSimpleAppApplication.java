f7cf23562b3d72cc9ebc6e668595d118587f20a6
shinjaeuk <nockdookong@gmail.com>
2020-03-21 16:04:48 +0900

    first commit

---
+++ src/main/java/com/spring/app/simple/SpringSimpleAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.app.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSimpleAppApplication.class, args);
+	}
+
+}
